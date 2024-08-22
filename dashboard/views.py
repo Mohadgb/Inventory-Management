2d8891b77252585f62779fbcb6a31043a8983763
Saïdi Mohamed <p0134047@chum.rtss.qc.ca>
2024-08-22 15:45:17 -0400

    Initial commit

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,18 +3,47 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Product
 from .forms import ProductForm
+from django.contrib.auth.models import User
 
 
 
 # Create your views here.
 
-@login_required
-def index(request):
-    return render(request, 'dashboard/index.html')
+#-------------------------------------------------
+# Index
+#-----------------------------------------------
 
 @login_required
-def staff(request):
+def index(request):
+    return render(request, 'dashboard/index.html',context)
+
+
+#---------------------------------------------------------
+# Customers
+#---------------------------------------------------------
+
+@login_required
+def staff(request): 
+    
+    
+    customer = User.objects.filter(groups=2)
+    customer_count = customer.count()
+    product = Product.objects.all()
+    product_count = product.count()
+    order = Order.objects.all()
+    order_count = order.count()
+    context = {
+        'customer': customer,
+        'customer_count': customer_count,
+        'product_count': product_count,
+        'order_count': order_count,
+    }
     return render(request, 'dashboard/staff.html')
+
+
+#=============================================================
+# Products
+#--------------------------------------------------------------
 
 @login_required
 def product(request):
@@ -33,6 +62,10 @@ def product(request):
     }
     return render(request, 'dashboard/product.html', context)
 
+#------------------------------------------------------------------------
+# Delete Product
+#-----------------------------------------------------------------------
+
 @login_required
 def product_delete(request, pk):
     item = Product.objects.get(id=pk) # To have it prefilled with the current info
@@ -44,6 +77,9 @@ def product_delete(request, pk):
     }
     return render(request, 'dashboard/products_delete.html', context)
 
+#----------------------------------------------------------------------------------
+# Update Product
+#-------------------------------------------------------------------------------
 
 @login_required
 def product_update(request, pk):
@@ -60,11 +96,9 @@ def product_update(request, pk):
     }
     return render(request, 'dashboard/products_update.html', context)
 
-
-
-
-
-
+#-------------------------------------------------------------------------
+# Order
+#-------------------------------------------------------------------------
 
 @login_required
 def order(request):
